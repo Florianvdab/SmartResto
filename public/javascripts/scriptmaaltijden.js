1bc1e09a9ec80d95062ffb518831b1cf0baee88a
Florian Vandenabeele <Florian.Vandenabeele@student.howest.be>
2019-05-26 11:23:13 +0200

    Winkelmand delete fix

--- public/javascripts/scriptmaaltijden.js
+++ public/javascripts/scriptmaaltijden.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", init);
 let prijs = 0;
 let totaal = 0;
-
+let nr = 0;
 function init() {
+    nr = 0;
     getMaaltijden().then(function (result) {
         displayMaaltijden(result);
     })
@@ -56,16 +57,45 @@ function generateMaaltijdListItem({ id, naam, prijs, ingredienten, keuken, url }
 }
 
 function winkelmand() {
+    nr++;
     prijs = this.parentElement.getElementsByClassName("prijs")[0].innerText.replace(/\€/g, '');
     totaal = eval(totaal) + eval(prijs);
-    document.getElementById("totaal").innerHTML ="€ " + totaal;
-    document.getElementById("winkelmandtotaal").innerHTML ="Totaal: € " + totaal;
-    document.getElementById("wtotaal").innerHTML ="Totaal: € " + totaal;
+    document.getElementById("totaal").innerHTML = "€ " + totaal;
+    document.getElementById("winkelmandtotaal").innerHTML = "<span class='totalider' style='font-weight:bold'>Totaal: € " + totaal + "</span>";
     let id = this.parentElement.getElementsByClassName("hiddenids")[0].innerText;
     let naam = this.parentElement.getElementsByClassName("naam")[0].innerText;
     let prijsmetteken = this.parentElement.getElementsByClassName("prijs")[0].innerText;
-    let samenvatting = "<span class='idss' style='display:none'>"+id+ "</span>" +naam + " - " + prijsmetteken;
-    document.getElementById("winkelmand").innerHTML +="<p>"+ samenvatting + "</p>";
-    document.getElementById("samenvatting").innerHTML +="<p>"+ samenvatting + "</p>";
-    
+    let samenvatting = "<span class='idss' style='display:none'>" + id + "</span>" + naam + " - " + "<span class='deleteprice'>" + prijsmetteken + "</span>";
+    document.getElementById("navbarDropdown").innerHTML = "Winkelmandje <span class='ml-2' id='nr' style='color:blue'>" + nr + "</span>"
+    document.getElementById("winkelmand").innerHTML += "<p class='deletethis'>" + samenvatting + "</p>";
+    document.getElementById("samenvatting").innerHTML = document.getElementById("winkelmand").innerHTML;
+    for (let i = 0; i < document.getElementById("samenvatting").getElementsByClassName("deletethis").length * 2; i++) {
+        document.getElementById("samenvatting").getElementsByClassName("deletethis")[0].className = "";
+    }
+    for (let i = 0; i < document.getElementsByClassName("deletethis").length; i++) {
+        document.getElementsByClassName("deletethis")[i].addEventListener("click", remove);
+    }
+
+}
+
+function remove() {
+    let newprice = this.getElementsByClassName("deleteprice")[0].innerText.replace(/\€/g, '');
+    this.innerHTML = "";
+    let old = document.getElementById("nr").innerText;
+    nr = eval(old) - 1;
+    document.getElementById("nr").innerText = nr;
+
+    let oldtotal = document.getElementById("totaal").innerText.replace(/\€/g, '');
+    let newtotal = parseInt(oldtotal) - parseInt(newprice);
+    totaal = newtotal;
+    console.log(oldtotal - newprice);
+    document.getElementById("totaal").innerText = "€ " + newtotal;
+    document.getElementById("winkelmandtotaal").innerHTML = "<span style='font-weight:bold'>Totaal: € " + newtotal + "</span>";
+    document.getElementById("samenvatting").innerHTML = document.getElementById("winkelmand").innerHTML;
+    console.log(document.getElementById("samenvatting").getElementsByClassName("deletethis"));
+
+
+
+
+
 }
